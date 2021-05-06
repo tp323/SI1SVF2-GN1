0c5e123da86e8a7436665dcd1dc196ed47344cc4
tp323 <tiagopardl@hotmail.com>
2021-05-06 23:54:21 +0100

    First
    removeTable done with drop if exists

---
+++ createTable.sql
@@ -0,0 +1,88 @@
+CREATE TABLE PASSAGEIRO (
+	nid char(10),
+	nome nvarchar(150),
+	dtnascimento date,
+	email nvarchar(60),
+	genero char(2)
+)
+
+
+CREATE TABLE LOCALIDADE (
+	codpostal int,
+	nome nvarchar(150)
+)
+
+CREATE TABLE AUTOCARROTIPO (
+	marca nchar(10),
+	modelo nchar(6),
+	nlugares tinyint
+)
+
+
+CREATE TABLE COMBOIOTIPO (
+	id char(2),
+	nome nchar(10),
+	nlugclasse1 tinyint,
+	nlugclasse2 tinyint
+)
+
+CREATE TABLE LUGARTIPO (
+	numero int,
+	nome nvarchar(10),
+	classe tinyint,
+	preço decimal(5,2)
+)
+
+CREATE TABLE ESTACAO (
+	
+)
+
+CREATE TABLE VIAGEM (
+	
+)
+
+CREATE TABLE RESERVA (
+	
+
+
+CREATE TABLE TRANSPORTE (
+	
+)
+
+CREATE TABLE AUTOCARRO (
+	
+)
+
+CREATE TABLE COMBOIO (
+	
+)
+
+CREATE TABLE LUGAR (
+	
+)
+
+CREATE TABLE PAGMBWAY (
+	
+)
+
+CREATE TABLE BILHETE (
+	
+)
+
+CREATE TABLE LOCOMOTIVA (
+	
+)
+
+CREATE TABLE ALFAPENDULAR (
+	
+)
+
+
+
+
+
+
+
+
+
+
